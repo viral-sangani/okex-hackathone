f53cd3c95fbeeb1926061f567715490c821a3512
Viral Sangani <virals@geekyants.com>
2021-09-10 17:59:12 +0530

    yes and no bet acceptance code added

--- contracts/PredictionMarketContract.sol
+++ contracts/PredictionMarketContract.sol
@@ -5,7 +5,7 @@ contract PredictionMarketContract {
     address public owner = msg.sender;
 
     mapping(uint256 => Questions) public questions;
-    uint256 public totalQuestions = 1;
+    uint256 public totalQuestions = 0;
 
     struct Questions {
         uint256 id;
@@ -14,8 +14,10 @@ contract PredictionMarketContract {
         address createdBy;
         string creatorImageHash;
         mapping(address => AmountAdded) yesCount;
-        AmountAdded[] noCount;
+        mapping(address => AmountAdded) noCount;
         uint256 totalAmount;
+        uint256 totalYesAmount;
+        uint256 totalNoAmount;
     }
 
     struct AmountAdded {
@@ -32,12 +34,13 @@ contract PredictionMarketContract {
         string creatorImageHash,
         AmountAdded[] yesCount,
         AmountAdded[] noCount,
-        uint256 totalAmount
+        uint256 totalAmount,
+        uint256 totalYesAmount,
+        uint256 totalNoAmount
     );
 
     function createQuestion(
         string memory _question,
-        address _createdBy,
         string memory _creatorImageHash
     ) public {
         AmountAdded[] memory yesCount;
@@ -49,20 +52,45 @@ contract PredictionMarketContract {
         question.id = totalQuestions;
         question.question = _question;
         question.timestamp = timestamp;
-        question.createdBy = _createdBy;
+        question.createdBy = msg.sender;
         question.creatorImageHash = _creatorImageHash;
         question.totalAmount = 0;
+        question.totalYesAmount = 0;
+        question.totalNoAmount = 0;
 
         emit QuestionCreated(
             totalQuestions,
             _question,
             timestamp,
-            _createdBy,
+            msg.sender,
             _creatorImageHash,
             yesCount,
             noCount,
+            0,
+            0,
             0
         );
-        totalQuestions++;
+    }
+
+    function addYesBet(uint256 _questionId) public payable {
+        Questions storage question = questions[_questionId];
+        AmountAdded storage amountAdded = question.yesCount[msg.sender];
+        amountAdded.timestamp = block.timestamp;
+        amountAdded.user = msg.sender;
+        amountAdded.amount = msg.value;
+        question.totalYesAmount += msg.value;
+        question.totalAmount += msg.value;
+    }
+
+    function addNoBet(uint256 _questionId) public payable {
+        Questions storage question = questions[_questionId];
+        AmountAdded storage amountAdded = question.noCount[msg.sender];
+        amountAdded.timestamp = block.timestamp;
+        amountAdded.user = msg.sender;
+        amountAdded.amount = msg.value;
+        question.totalNoAmount += msg.value;
+        question.totalAmount += msg.value;
     }
 }
+
+// "Will Modi win?", "abcd"
